9fe49c4e75d434aa9f846ad6f6bd0824e1256afe
秋雨落 <i@rain.cx>
2021-08-23 17:37:33 +0800

    :sparkles: Add annotations. WIP.
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/cx/rain/mc/leanglecore/annotation/ModItem.java
@@ -0,0 +1,12 @@
+package cx.rain.mc.leanglecore.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ModItem {
+    public String key();
+}
